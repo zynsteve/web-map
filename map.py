4a2035507173e04ccf6b98d48aa970d51e2a6cbc
Yinan Zhu <zynsteve@tamu.edu>
2017-06-16 10:07:36 -0400

    add population feature

--- map.py
+++ map.py
@@ -14,13 +14,17 @@ def color_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[36, -96],zoom_start=4.3, tiles="Mapbox Bright")
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My map")
+fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=str(el)+" m",
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m",
     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005']<10000000
+else 'orange' if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
 
 map.add_child(fg)
 
