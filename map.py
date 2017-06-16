5e7e2e6d80b3a8baf05035ce3b25ddb16aa0e947
Yinan Zhu <zynsteve@tamu.edu>
2017-06-16 11:02:07 -0400

    add world volcano data

--- map.py
+++ map.py
@@ -1,10 +1,11 @@
 import folium
 import pandas
 
-data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-elev = list(data["ELEV"])
+data = pandas.read_csv("world_volcanoes.csv")
+lat = list(data["Latitude"])
+lon = list(data["Longitude"])
+elev = list(data["Elev"])
+name = list(data['Volcano Name'])
 
 def color_producer(elevation):
     if elevation<1000:
@@ -18,8 +19,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fgv = folium.FeatureGroup(name="Volcanoes")
 
-for lt, ln, el in zip(lat, lon, elev):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m",
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=4, popup=name+", "+str(el)+"m",
     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 fgp = folium.FeatureGroup(name="Poputation")
