55e0c4e17c5f9300c1cbad15ed545319ccb104e2
Yinan Zhu <zynsteve@tamu.edu>
2017-06-16 09:48:23 -0400

    add and style circle markers

--- map.py
+++ map.py
@@ -6,12 +6,21 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000<=elevation<3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[36, -96],zoom_start=4.3, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=str(el)+" m",
+    fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 map.add_child(fg)
 
